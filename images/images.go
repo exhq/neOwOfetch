1c02e71c47b8e30a769690b4f789bb4a5e26403c
echo <91651232+exhq@users.noreply.github.com>
2022-09-25 12:45:00 +0330

    fixed images, support for having other distro's asciiart/png

--- images/images.go
+++ images/images.go
@@ -8,7 +8,11 @@ var ArchArt []byte
 //go:embed ubuntu.jpg
 var UbuntuArt []byte
 
+//go:embed saul.png
+var saulArt []byte
+
 var DistroImages = map[string][]byte{
 	"arch":   ArchArt,
+	"saul":   saulArt,
 	"ubuntu": UbuntuArt,
 }
