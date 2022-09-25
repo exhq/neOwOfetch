7e760950db5ff8f183b56a0a41f309ee86a796fc
echo <91651232+exhq@users.noreply.github.com>
2022-09-25 13:57:18 +0330

    yeah

--- utils/args.go
+++ utils/args.go
@@ -13,16 +13,24 @@ var usepng bool = false
 var hascolor bool = true
 var Customascii = false
 var asciidir string
+var asciiforced = false
+var forceddistro string
 
+func Gethn() string {
+	cmd := exec.Command("whoami")
+	shell, _ := cmd.Output()
+	return strings.Replace(string(shell), "\n", "", -1)
+}
 func Initargs() {
 	args = os.Args[1:]
 	for _, argument := range args {
 		if strings.HasPrefix(argument, "--ascii=") {
-			cmd := exec.Command("whoami")
-			shell, _ := cmd.Output()
-			funny := strings.Replace(string(shell), "\n", "", -1)
 			Customascii = true
-			asciidir = strings.ReplaceAll(argument[8:], "~", "/home/"+funny)
+			asciidir = strings.ReplaceAll(argument[8:], "~", "/home/"+Gethn())
+		}
+		if strings.HasPrefix(argument, "--distro=") {
+			asciiforced = true
+			forceddistro = argument[9:]
 		} else if strings.HasPrefix(argument, "--") {
 			switch argument {
 			case "--nouwu":
